06e8499bc63ac2a8b81fd051007fdf5b565ca8fd
khelsayed <khelsayed73@gmail.com>
2023-09-03 07:23:34 +0300

    organize

--- python/countingItemInList.py
+++
@@ -1,7 +0,0 @@
-import random
-
-size = random.randint(1, 100)                                # pick a random integer between 1 and 100 to be size of the list
-mylist = [random.randint(1, 10) for _ in range(size)]        # pick a random integer between 1 and 10 to fill every element within the size of the list
-total = mylist.count(4)
-print("Generated List:", mylist)                            # print the list
-print("Count of 4:", total)                                 # print the count
