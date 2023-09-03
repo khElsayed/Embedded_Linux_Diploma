cc76867fc6218f20d7cf3d49ffb936ad75da95c6
khelsayed <khelsayed73@gmail.com>
2023-09-03 07:17:49 +0300

    simple isVowel python_1

---
+++ 01_python/01_introduction/01_Task/isVowel.py
@@ -0,0 +1,5 @@
+letter = 'k'
+if letter == 'a' or letter == 'o' or letter == 'u' or letter == "i" or letter == 'e':
+    print("The letter is a vowel")
+else:
+    print("The letter is not a vowel")
