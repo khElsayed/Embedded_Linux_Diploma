7845aaa64f98c38a378e0a5d9536249a88adb79c
khelsayed <khelsayed73@gmail.com>
2023-09-08 01:07:48 +0300

    organized asciiTable

--- 02_c++/01_introduction/01_Task/asciiTable.cpp
+++ 02_c++/01_introduction/01_Task/asciiTable.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
+#include <iomanip>      // used to manipulate input and output 
 using namespace std;
 
 int main()
 {
     cout << "ASCII Code Table:\n";
-    cout << "+--------+---------+\n";
-    cout << "|  char  |  ASCII  |\n";
-    cout << "+--------+---------+\n";
-    for(int i=0; i<128; i++)
+    cout << "+----------+----------+\n";
+    cout << "|   Char   |  ASCII   |\n";
+    cout << "+----------+----------+\n";
+    for (int i = 0; i < 128; i++)
     {
-        char x = char(i);
-        cout << "|    " << x << "  |   " << i <<  "   |\n";    
+        string charName;
+        switch (i)
+        {
+            case 0: charName = "NULL"; 
+                break;
+            case 7: charName = "BEL"; 
+                break;
+            case 8: charName = "BKSP"; 
+                break;
+            case 9: charName = "TAB"; 
+                break;
+            case 10: charName = "LiFe"; 
+                break;
+            case 13: charName = "CR"; 
+                break;
+            default: charName = ""; 
+                break;
+        }
+        cout << "|   " << setw(4) << left << (charName.empty() ? string(1, i) : charName) <<  "   |   " << setw(4) << i << "   |\n";
     }
     return 0;
 }
