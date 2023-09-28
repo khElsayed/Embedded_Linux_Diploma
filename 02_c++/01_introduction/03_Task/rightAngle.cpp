430a49c250ccc95e0c3cec3f6f1c729da3ba947b
khelsayed <khelsayed73@gmail.com>
2023-09-29 02:34:31 +0300

    basic rightAngleTriangle bug fixed

--- 02_c++/01_introduction/03_Task/rightAngle.cpp
+++ 02_c++/01_introduction/03_Task/rightAngle.cpp
@@ -13,6 +13,8 @@ int main()
     std::cout << "Enter length of side C: " << std::endl;
     std::cin >> c;
 
+    if((a != 0) && (b != 0) && (c != 0))
+    {
     if((a*a) == (b*b) + (c*c))
         angle = a;
     else if((b*b) == (a*a) + (c*c))
@@ -21,6 +23,7 @@ int main()
         angle = c;
     else
         angle = 0;
+    }
     
     if(angle == a)
         std::cout << "The angle between B and C is 90 degree, hence it is a right angle triangle" << std::endl;
