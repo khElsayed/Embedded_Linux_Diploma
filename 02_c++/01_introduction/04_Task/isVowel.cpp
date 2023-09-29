384393163da983c890662df35f7d39fe7fd445bc
khelsayed <khelsayed73@gmail.com>
2023-09-29 03:07:26 +0300

    find isVowel

--- 02_c++/01_introduction/04_Task/isVowel.cpp
+++ 02_c++/01_introduction/04_Task/isVowel.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
     char myLetter = '0';
+    std::string vowelLetters = "aeiouAEIOU";
     std::cout << "Enter a letter: " << std::endl;
     std::cin >> myLetter;
-    if((myLetter == 'a') || (myLetter == 'e') || (myLetter == 'i') || (myLetter == 'o') || (myLetter == 'u'))
-        std::cout << "The letter you have entered is a vowel " << std::endl;
-    else if((myLetter == 'A') || (myLetter == 'E') || (myLetter == 'I') || (myLetter == 'O') || (myLetter == 'U'))
+    int result = vowelLetters.find(myLetter);       // if char exist in the string, result will be the number of char inside the string, else it will return -1
+    if(result != -1)
         std::cout << "The letter you have entered is a vowel " << std::endl;
     else
         std::cout << "The letter you have entered is not a vowel " << std::endl;
---
+++ 02_c++/01_introduction/04_Task/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+    std::cout << answer << std::endl;
